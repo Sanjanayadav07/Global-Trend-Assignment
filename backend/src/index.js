458c475fbaf83d0c764b5030ba52f9457e0a34f9
sanjanayadav07 <sanjanayadav3952@gmail.com>
2026-04-10 21:12:28 +0530

    fix cors for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,9 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://global-trend-assignment-navy.vercel.app",
+    ],
     credentials: true,
-  }),
+  })
 );
 
 app.use(express.json());
